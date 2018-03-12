0d6e87777e9cbd44e241a61d6ceac318094a9122
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-03-12 18:37:25 +0500

    khan

---
+++ Alpha_Gps_Location_26_2_18/app/src/main/java/com/zaaviyah/alpha_gps_location_26_2_18/MainActivity.java
@@ -0,0 +1,46 @@
+package com.zaaviyah.alpha_gps_location_26_2_18;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1Loction;
+
+    GPSTracker gps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1Loction=(Button)findViewById(R.id.btLocation);
+
+
+        b1Loction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                gps = new GPSTracker(MainActivity.this);
+
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+    }
+}
